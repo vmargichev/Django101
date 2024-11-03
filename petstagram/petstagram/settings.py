0f33c060833e34f06f4b2d17af0e3967a7d914bd
vmargichev <93369733+vmargichev@users.noreply.github.com>
2024-11-03 16:03:05 +0200

    Fixed collectstatic

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
